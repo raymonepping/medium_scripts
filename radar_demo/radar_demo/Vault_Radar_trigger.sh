feb83565e68ee7692c8880dcf4e248a741f9ad83
Raymon Epping <84765507+raymonepping@users.noreply.github.com>
2025-07-05 10:41:02 +0200

    Update Vault_Radar_trigger.sh

--- radar_demo/radar_demo/Vault_Radar_trigger.sh
+++ radar_demo/radar_demo/Vault_Radar_trigger.sh
@@ -1,16 +1,2 @@
-###############################################################
-# Vault Radar Demo Leak Seed
-# Version: 1.0.1
-# Timestamp: 2025-07-05 10:36:45
-# Run ID: 1751704605-13604
-# Author: raymon.epping
-# Scenario: AWS
-###############################################################
-#!/bin/bash
-# [secret][high] AWS Access Key
-AWS_ACCESS_KEY_ID=AKIA1234567890FAKE
-
-###############################################################
-# End of Vault Radar Demo Leak Seed
-# For educational/demo use only!
-###############################################################
+AWS_ACCESS_KEY_ID=AKIAZZZZZZZZZZZZZZZZZ
+AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYRANDOMKEY
