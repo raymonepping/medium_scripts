947e99c4a9a022222db0d293365c2c135b6f648b
Raymon Epping <your@email.com>
2025-07-05 07:43:29 +0200

    05/07/2025 - Updated configuration and fixed bugs

--- top10_validator.sh
+++ top10_validator.sh
@@ -3,8 +3,8 @@
 
 # --- top10_validator.sh: Check a script against our Bash Top 10 (Color + Markdown) ---
 
-# shellcheck disable=SC2034 # intentional: VERSION is used by automation
-VERSION="2.1.0"
+# shellcheck disable=SC2034
+VERSION="2.1.2"
 
 # --- Enable strict mode for safety and fail-fast behavior ---
 set -euo pipefail
