91470902fa21b03aa9dbf048555f8b0998663de0
Raymon Epping <your@email.com>
2025-07-05 10:10:08 +0200

    05/07/2025 - Updated configuration and fixed bugs

---
+++ Vault_Radar_trigger.py
@@ -0,0 +1,16 @@
+###############################################################
+# Vault Radar Demo Leak Seed
+# Version: 1.0.1
+# Timestamp: 2025-07-05 09:43:12
+# Run ID: 1751701392-3588
+# Author: raymon.epping
+# Scenario: github
+###############################################################
+# Python leak demo
+# [secret][high] GitHub Token
+GitHub Token = "GITHUB_TOKEN=ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+
+###############################################################
+# End of Vault Radar Demo Leak Seed
+# For educational/demo use only!
+###############################################################
